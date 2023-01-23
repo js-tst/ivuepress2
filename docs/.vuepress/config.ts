73eb6965f1fcab4f005d6388706c983ceab2d249
js-tst <72301979+js-tst@users.noreply.github.com>
2023-01-23 22:57:22 +0800

    +reco&success

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,14 +1,21 @@
-import {defaultTheme, defineUserConfig} from 'vuepress'
+import { defineUserConfig } from 'vuepress'
+import { recoTheme } from 'vuepress-theme-reco'
 
 export default defineUserConfig({
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
-    description: '这是我的第一个 VuePress 站点',
+    title: '碱式碳酸铜的乐色桶',
+    description: '这里装着碱式碳酸铜生产的拉机',
 
-    theme: defaultTheme({
+    theme: recoTheme({
         // Public 文件路径
         logo: '/images/icon.png',
         // URL
         //logo: 'https://vuejs.org/images/logo.png',
+
+        autoSetBlogCategories: true,
+        autoAddCategoryToNavbar: true,
+
+        author: '碱式碳酸铜',
+        authorAvatar: '/images/head.png'
     }),
 })
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,10 @@
-# Hello VuePress
+---
+home: true
+modules: # 指定首页展示模块
+    - Banner
+    - Blog
+banner: # banner 模块的配置
+    heroText: 碱式碳酸铜的乐色桶
+    tagline: 这里装着碱式碳酸铜生产的拉机
+    bgImage: /images/bg.jpg
+---
