da384728d5dc83e2de28d8fd505e59f3cf64244c
js-tst <72301979+js-tst@users.noreply.github.com>
2023-01-26 16:25:51 +0800

    +百度统计

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,9 +2,17 @@ import { defineUserConfig } from 'vuepress'
 import { recoTheme } from 'vuepress-theme-reco'
 
 export default defineUserConfig({
-    lang: 'zh-CN',
-    title: '碱式碳酸铜的乐色桶',
     description: '这里装着碱式碳酸铜生产的拉机',
+    head: [["script", {}, `
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?fee96d561d8e7513befc2d9cb32d0949";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+    `,]],
+    lang: 'zh-CN',
 
     theme: recoTheme({
         // Public 文件路径
@@ -18,4 +26,6 @@ export default defineUserConfig({
         author: '碱式碳酸铜',
         authorAvatar: '/images/head.png'
     }),
+
+    title: '碱式碳酸铜的乐色桶',
 })
